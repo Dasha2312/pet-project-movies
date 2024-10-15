d9fadb4f3695b61bfbe3e6a7d8c0b4ba8642d5c0
Dasha <dasha2312s3@gmail.com>
2024-10-15 14:33:59 +0300

    changed toast style

--- src/hooks/useAddToWatch.js
+++ src/hooks/useAddToWatch.js
@@ -9,7 +9,19 @@ function useAddToWatch() {
     onSuccess: (response) => {
       // toast.success('You add movie to list Watch Later');
       if (response.deleted) {
-        toast.success('This movie had been deleted from your Watch Later list');
+        // toast.success('This movie had been deleted from your Watch Later list');
+        toast.success('This movie had been deleted from your Watch Later list.', {
+          style: {
+            border: '1px solid ##ffe69c',
+            background: '#fff3cd',
+            padding: '16px',
+            color: '#664d03',
+          },
+          iconTheme: {
+            primary: '#664d03',
+            secondary: '#fff',
+          },
+        });
       } else {
         toast.success('You have just added a movie to your Watch Later list');
       }

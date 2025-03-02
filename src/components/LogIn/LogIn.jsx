abfbacf822e7662fb8f920e031bab9e963140549
Dasha <dasha2312s3@gmail.com>
2025-03-02 17:58:16 +0200

    changed styles

--- src/components/LogIn/LogIn.jsx
+++ src/components/LogIn/LogIn.jsx
@@ -2,41 +2,43 @@ import { Box, Modal, TextField } from "@mui/material";
 import { Controller, useForm } from "react-hook-form";
 import { useLogIn } from "../../hooks/Auth/useLogIn";
 
-import LoadingButton from '@mui/lab/LoadingButton';
+import LoadingButton from "@mui/lab/LoadingButton";
 import { useDispatch, useSelector } from "react-redux";
 import { closeAuthModal } from "../../store/authModalSlice";
 
-
-function LogIn({ setOpenModalSingUp}) {
-  
-  const {login, isPending} = useLogIn();
+function LogIn({ setOpenModalSingUp }) {
+  const { login, isPending } = useLogIn();
   const dispatch = useDispatch();
-  const openLogIn = useSelector(state => state.authModal.isOpen);
+  const openLogIn = useSelector((state) => state.authModal.isOpen);
 
-  const {reset, handleSubmit, formState: {errors}, control} = useForm({
+  const {
+    reset,
+    handleSubmit,
+    formState: { errors },
+    control,
+  } = useForm({
     defaultValues: {
-      email: '',
-      password: ''
-    }
-  })
+      email: "",
+      password: "",
+    },
+  });
 
   function handleSingUpModal() {
     setOpenModalSingUp(true);
-    dispatch(closeAuthModal())
+    dispatch(closeAuthModal());
   }
 
   function onSubmit(data) {
-    console.log("log in ",data)
+    console.log("log in ", data);
     login(data, {
       onSettled: () => {
-        reset()
+        reset();
       },
-      onSuccess:() => {
-        dispatch(closeAuthModal())
-        reset()
-      }
-    })
-    
+      onSuccess: () => {
+        dispatch(closeAuthModal());
+        reset();
+      },
+    });
   }
 
   return (
@@ -45,104 +47,144 @@ function LogIn({ setOpenModalSingUp}) {
       onClose={() => dispatch(closeAuthModal())}
       sx={{
         ".MuiBackdrop-root": {
-          backgroundColor: "rgba(0, 0, 0, 0.85)"
-        }
+          backgroundColor: "rgba(0, 0, 0, 0.85)",
+        },
       }}
     >
-      <Box className="modal-block">
-        <Box component="h2" sx={{textAlign: "center", marginBottom: '20px'}}>Log In</Box>
+      <Box className="modal-block" sx={{width: "90%", maxWidth: "530px", minWidth: "auto", maxHeight: "90vh", overflowY: "auto", padding: {xs: "20px", md: "40px"}}}>
+        <Box component="h2" sx={{ textAlign: "center", marginBottom: "20px" }}>
+          Log In
+        </Box>
         <form onSubmit={handleSubmit(onSubmit)} autoComplete="off">
-          <Box sx={{marginBottom: "20px"}}>
-            <Box className="form-label">Your Email</Box>
-            <Controller
-              name="email"
-              control={control}
-              rules={{
-                required: 'Email is required',
-                validate: {
-                  notEmpty: value => value.trim() !== "" || "Email cannot be empty",
-                  validEmail: value => /\S+@\S+\.\S+/.test(value) || 'Please enter a valid email address'
-                }
-              }}
-              render={({field}) => (
-                <>
-                  <TextField variant="outlined" {...field} fullWidth 
-                    error={!!errors.email}
-                    disabled={isPending}
-                    sx={{
-                      "& .MuiInputBase-input ": {
-                        color: "#fff",
-                        padding: "20px",
-                        border: 0,
-                        minHeight: "55px"
-                      },
-                      "& fieldset, &:hover fieldset": {
-                        border: '1px solid #333 !important',
-                        borderRadius: "8px", 
-                      },
-                      "& .Mui-focused .MuiOutlinedInput-notchedOutline": {
-                        border: '1px solid #fff !important',
-                      },
-                      "& .Mui-error fieldset": {
-                        border: "1px solid #e50000 !important"
-                      }
-                    }}
-                    helperText={errors.email && errors.email.message}
-                  />
-                </>
-              )}
-            />
-            
+          <Box sx={{ marginBottom: "40px" }}>
+            <Box sx={{ marginBottom: "20px" }}>
+              <Box className="form-label">Your Email</Box>
+              <Controller
+                name="email"
+                control={control}
+                rules={{
+                  required: "Email is required",
+                  validate: {
+                    notEmpty: (value) =>
+                      value.trim() !== "" || "Email cannot be empty",
+                    validEmail: (value) =>
+                      /\S+@\S+\.\S+/.test(value) ||
+                      "Please enter a valid email address",
+                  },
+                }}
+                render={({ field }) => (
+                  <>
+                    <TextField
+                      variant="outlined"
+                      {...field}
+                      fullWidth
+                      error={!!errors.email}
+                      disabled={isPending}
+                      sx={{
+                        "& .MuiInputBase-input ": {
+                          color: "#fff",
+                          padding: "20px",
+                          border: 0,
+                          minHeight: "55px",
+                        },
+                        "& fieldset, &:hover fieldset": {
+                          border: "1px solid #333 !important",
+                          borderRadius: "8px",
+                        },
+                        "& .Mui-focused .MuiOutlinedInput-notchedOutline": {
+                          border: "1px solid #fff !important",
+                        },
+                        "& .Mui-error fieldset": {
+                          border: "1px solid #e50000 !important",
+                        },
+                      }}
+                      helperText={errors.email && errors.email.message}
+                    />
+                  </>
+                )}
+              />
+            </Box>
+            <Box sx={{ marginBottom: "20px" }}>
+              <Box className="form-label">Your password</Box>
+              <Controller
+                control={control}
+                name="password"
+                rules={{
+                  required: "Password is required",
+                  validate: {
+                    notEmpty: (value) =>
+                      value.trim() !== "" || "Password cannot be empty",
+                  },
+                }}
+                render={({ field }) => (
+                  <>
+                    <TextField
+                      variant="outlined"
+                      {...field}
+                      fullWidth
+                      error={!!errors.password}
+                      disabled={isPending}
+                      sx={{
+                        "& .MuiInputBase-input ": {
+                          color: "#fff",
+                          padding: "20px",
+                          border: 0,
+                          minHeight: "55px",
+                        },
+                        "& fieldset, &:hover fieldset": {
+                          border: "1px solid #333 !important",
+                          borderRadius: "8px",
+                        },
+                        "& .Mui-focused .MuiOutlinedInput-notchedOutline": {
+                          border: "1px solid #fff !important",
+                        },
+                        "& .Mui-error fieldset": {
+                          border: "1px solid #e50000 !important",
+                        },
+                      }}
+                      helperText={errors.password && errors.password.message}
+                    />
+                  </>
+                )}
+              />
+            </Box>
           </Box>
-          <Box sx={{marginBottom: "20px"}}>
-            <Box className="form-label">Your password</Box>
-            <Controller
-              control={control}
-              name="password"
-              rules={{
-                required: "Password is required",
-                validate: {
-                  notEmpty: value => value.trim() !== "" || "Password cannot be empty",
-                }
-              }}
-              render={({field}) => (
-                <>
-                  <TextField variant="outlined" {...field} fullWidth
-                    error={!!errors.password}
-                    disabled={isPending}
-                    sx={{
-                      "& .MuiInputBase-input ": {
-                        color: "#fff",
-                        padding: "20px",
-                        border: 0,
-                        minHeight: "55px"
-                      },
-                      "& fieldset, &:hover fieldset": {
-                        border: '1px solid #333 !important',
-                        borderRadius: "8px", 
-                      },
-                      "& .Mui-focused .MuiOutlinedInput-notchedOutline": {
-                        border: '1px solid #fff !important',
-                      },
-                      "& .Mui-error fieldset": {
-                        border: "1px solid #e50000 !important"
-                      }
-                    }}
-                    helperText={errors.password && errors.password.message}
-                  />
-                </>
-              )}
-            />
-          </Box>
-          <LoadingButton type="submit" className="btnRed" variant="contained" fullWidth loading={isPending}>
+          <LoadingButton
+            type="submit"
+            className="btnRed"
+            sx={{
+              bgcolor: "#e50000",
+              p: "10px",
+              fontWeight: "600",
+              fontSize: "16px",
+              "&.MuiLoadingButton-loading": {
+                bgcolor: "#e50000",
+              },
+              "& .MuiCircularProgress-root": {
+                width: "20px !important",
+                height: "20px !important"
+              },
+              "& .MuiCircularProgress-svg": {
+                // color: "#fff"
+              }
+            }}
+            variant="contained"
+            fullWidth
+            loading={isPending}
+          >
             Send
           </LoadingButton>
         </form>
 
-        <Box sx={{marginTop: "30px"}}>
-          <Box sx={{textAlign: "center"}}>Don’t have an account?</Box>
-          <Box sx={{marginTop: "10px"}}>
-            <button onClick={handleSingUpModal} className="btnText text-align-center w-100">Sing Up</button>
+        <Box sx={{ marginTop: "30px" }}>
+          <Box sx={{ textAlign: "center" }}>Don’t have an account?</Box>
+          <Box sx={{ marginTop: "10px" }}>
+            <button
+              onClick={handleSingUpModal}
+              className="btnText text-align-center w-100"
+            >
+              Sing Up
+            </button>
           </Box>
         </Box>
       </Box>
--- src/components/SingUpForm/SingUpForm.jsx
+++ src/components/SingUpForm/SingUpForm.jsx
@@ -1,24 +1,28 @@
-import { Box, Modal, TextField } from '@mui/material';
-import { Controller, useForm } from 'react-hook-form';
-import { useSingUp } from '../../hooks/Auth/useSingUp';
-import { LoadingButton } from '@mui/lab';
+import { Box, Modal, TextField } from "@mui/material";
+import { Controller, useForm } from "react-hook-form";
+import { useSingUp } from "../../hooks/Auth/useSingUp";
+import { LoadingButton } from "@mui/lab";
 
-function SingUpForm({isSignUpOpen, closeSingUpModal, handleCloseModal}) {
-  const {singup, isPending} = useSingUp();
+function SingUpForm({ isSignUpOpen, closeSingUpModal, handleCloseModal }) {
+  const { singup, isPending } = useSingUp();
 
-  const {handleSubmit, formState: { errors }, reset, control} = useForm()
+  const {
+    handleSubmit,
+    formState: { errors },
+    reset,
+    control,
+  } = useForm();
 
-
-  function onSubmit({fullName, email, password}) {
+  function onSubmit({ fullName, email, password }) {
     singup(
-      {fullName, email, password},
+      { fullName, email, password },
       {
         onSettled: () => {
           reset();
-          closeSingUpModal
+          closeSingUpModal;
         },
       }
-    )
+    );
   }
 
   return (
@@ -27,149 +31,191 @@ function SingUpForm({isSignUpOpen, closeSingUpModal, handleCloseModal}) {
       onClose={closeSingUpModal}
       sx={{
         ".MuiBackdrop-root": {
-          backgroundColor: "rgba(0, 0, 0, 0.85)"
-        }
+          backgroundColor: "rgba(0, 0, 0, 0.85)",
+        },
       }}
     >
-      <Box className="modal-block">
-        <Box component="h2" sx={{textAlign: "center", marginBottom: '20px'}}>Sing Up</Box>
+      <Box className="modal-block" sx={{width: "90%", maxWidth: "530px", minWidth: "auto", maxHeight: "90vh", overflowY: "auto", padding: {xs: "20px", md: "40px"}}}>
+        <Box component="h2" sx={{ textAlign: "center", marginBottom: "20px" }}>
+          Sing Up
+        </Box>
         <form onSubmit={handleSubmit(onSubmit)} autoComplete="off">
-          <Box sx={{marginBottom: "20px"}}>
-            <Box className="form-label">Your Name</Box>
-            <Controller
-              name="fullName"
-              control={control}
-              rules={{
-                required: 'User name is required',
-                validate: value => value.trim() !== '' || 'Name cannot be empty'
-              }}
-              render={({field}) => (
-                <>
-                  <TextField variant="outlined" {...field} fullWidth
-                    sx={{
-                      "& .MuiInputBase-input ": {
-                        color: "#fff",
-                        padding: "20px",
-                        border: 0,
-                        minHeight: "55px"
-                      },
-                      "& fieldset, &:hover fieldset": {
-                        border: '1px solid #333 !important',
-                        borderRadius: "8px", 
-                      },
-                      "& .Mui-focused .MuiOutlinedInput-notchedOutline": {
-                        border: '1px solid #fff !important',
-                      },
-                      "& .Mui-error fieldset": {
-                        border: "1px solid #e50000 !important"
-                      }
-                    }}
-                    error={!!errors.fullName}
-                    helperText={errors.fullName && errors.fullName.message}
-                  />
-                </>
-              )}
-            />            
-          </Box>
-          <Box sx={{marginBottom: "20px"}}>
-            <Box className="form-label">Your Email</Box>
-            <Controller
-              name="email"
-              control={control}
-              rules={{
-                required: 'Email is required',
-                validate: {
-                  notEmpty: value => value.trim() !== '' || 'Email cannot be empty',
-                  validEmail: value => /\S+@\S+\.\S+/.test(value) || 'Please enter a valid email address'
-                }
-              }}
-              render={({field}) => (
-                <>
-                  <TextField variant="outlined" {...field} fullWidth
-                    sx={{
-                      "& .MuiInputBase-input ": {
-                        color: "#fff",
-                        padding: "20px",
-                        border: 0,
-                        minHeight: "55px"
-                      },
-                      "& fieldset, &:hover fieldset": {
-                        border: '1px solid #333 !important',
-                        borderRadius: "8px", 
-                      },
-                      "& .Mui-focused .MuiOutlinedInput-notchedOutline": {
-                        border: '1px solid #fff !important',
-                      },
-                      "& .Mui-error fieldset": {
-                        border: "1px solid #e50000 !important"
-                      }
-                    }}
-                    error={!!errors.email}
-                    helperText={errors.email && errors.email.message}
-                  />
-                </>
-              )}
-            />            
-          </Box>
-          <Box sx={{marginBottom: "20px"}}>
-            <Box className="form-label">Your Password</Box>
-            <Controller
-              name="password"
-              control={control}
-              rules={{
-                required: 'Password is required',
-                minLength: {
-                  value: 6,
-                  message: 'Password must be at least 6 characters long'
-                },
-                validate: value => value.trim() !== '' || 'Password cannot be empty',
-              }}
-              render={({field}) => (
-                <>
-                  <TextField variant="outlined" {...field} fullWidth
-                    sx={{
-                      "& .MuiInputBase-input ": {
-                        color: "#fff",
-                        padding: "20px",
-                        border: 0,
-                        minHeight: "55px"
-                      },
-                      "& fieldset, &:hover fieldset": {
-                        border: '1px solid #333 !important',
-                        borderRadius: "8px", 
-                      },
-                      "& .Mui-focused .MuiOutlinedInput-notchedOutline": {
-                        border: '1px solid #fff !important',
-                      },
-                      "& .Mui-error fieldset": {
-                        border: "1px solid #e50000 !important"
-                      }
-                    }}
-                    error={!!errors.password}
-                    helperText={errors.password && errors.password.message}
-                  />
-                </>
-              )}
-            />
+          <Box sx={{ marginBottom: "40px" }}>
+            <Box sx={{ marginBottom: "20px" }}>
+              <Box className="form-label">Your Name</Box>
+              <Controller
+                name="fullName"
+                control={control}
+                rules={{
+                  required: "User name is required",
+                  validate: (value) =>
+                    value.trim() !== "" || "Name cannot be empty",
+                }}
+                render={({ field }) => (
+                  <>
+                    <TextField
+                      variant="outlined"
+                      {...field}
+                      fullWidth
+                      sx={{
+                        "& .MuiInputBase-input ": {
+                          color: "#fff",
+                          padding: "20px",
+                          border: 0,
+                          minHeight: "55px",
+                        },
+                        "& fieldset, &:hover fieldset": {
+                          border: "1px solid #333 !important",
+                          borderRadius: "8px",
+                        },
+                        "& .Mui-focused .MuiOutlinedInput-notchedOutline": {
+                          border: "1px solid #fff !important",
+                        },
+                        "& .Mui-error fieldset": {
+                          border: "1px solid #e50000 !important",
+                        },
+                      }}
+                      error={!!errors.fullName}
+                      helperText={errors.fullName && errors.fullName.message}
+                    />
+                  </>
+                )}
+              />
+            </Box>
+            <Box sx={{ marginBottom: "20px" }}>
+              <Box className="form-label">Your Email</Box>
+              <Controller
+                name="email"
+                control={control}
+                rules={{
+                  required: "Email is required",
+                  validate: {
+                    notEmpty: (value) =>
+                      value.trim() !== "" || "Email cannot be empty",
+                    validEmail: (value) =>
+                      /\S+@\S+\.\S+/.test(value) ||
+                      "Please enter a valid email address",
+                  },
+                }}
+                render={({ field }) => (
+                  <>
+                    <TextField
+                      variant="outlined"
+                      {...field}
+                      fullWidth
+                      sx={{
+                        "& .MuiInputBase-input ": {
+                          color: "#fff",
+                          padding: "20px",
+                          border: 0,
+                          minHeight: "55px",
+                        },
+                        "& fieldset, &:hover fieldset": {
+                          border: "1px solid #333 !important",
+                          borderRadius: "8px",
+                        },
+                        "& .Mui-focused .MuiOutlinedInput-notchedOutline": {
+                          border: "1px solid #fff !important",
+                        },
+                        "& .Mui-error fieldset": {
+                          border: "1px solid #e50000 !important",
+                        },
+                      }}
+                      error={!!errors.email}
+                      helperText={errors.email && errors.email.message}
+                    />
+                  </>
+                )}
+              />
+            </Box>
+            <Box sx={{ marginBottom: "20px" }}>
+              <Box className="form-label">Your Password</Box>
+              <Controller
+                name="password"
+                control={control}
+                rules={{
+                  required: "Password is required",
+                  minLength: {
+                    value: 6,
+                    message: "Password must be at least 6 characters long",
+                  },
+                  validate: (value) =>
+                    value.trim() !== "" || "Password cannot be empty",
+                }}
+                render={({ field }) => (
+                  <>
+                    <TextField
+                      variant="outlined"
+                      {...field}
+                      fullWidth
+                      sx={{
+                        "& .MuiInputBase-input ": {
+                          color: "#fff",
+                          padding: "20px",
+                          border: 0,
+                          minHeight: "55px",
+                        },
+                        "& fieldset, &:hover fieldset": {
+                          border: "1px solid #333 !important",
+                          borderRadius: "8px",
+                        },
+                        "& .Mui-focused .MuiOutlinedInput-notchedOutline": {
+                          border: "1px solid #fff !important",
+                        },
+                        "& .Mui-error fieldset": {
+                          border: "1px solid #e50000 !important",
+                        },
+                      }}
+                      error={!!errors.password}
+                      helperText={errors.password && errors.password.message}
+                    />
+                  </>
+                )}
+              />
+            </Box>
           </Box>
           <Box>
-            {/* <Button type="submit" className="btnRed" variant="contained"  fullWidth>Send</Button> */}
-            <LoadingButton type="submit" className="btnRed" variant="contained"  fullWidth loading={isPending}>
-            Send
-          </LoadingButton>
+            <LoadingButton
+              sx={{
+                bgcolor: "#e50000",
+                p: "10px",
+                fontWeight: "600",
+                fontSize: "16px",
+                "&.MuiLoadingButton-loading": {
+                  bgcolor: "#e50000",
+                },
+                "& .MuiCircularProgress-root": {
+                  width: "20px !important",
+                  height: "20px !important"
+                },
+                "& .MuiCircularProgress-svg": {
+                  // color: "#fff"
+                }
+              }}
+              type="submit"
+              className="btnRed"
+              variant="contained"
+              fullWidth
+              loading={isPending}
+            >
+              Send
+            </LoadingButton>
           </Box>
         </form>
-            
-        <Box sx={{marginTop: "30px"}}>
-          <Box sx={{textAlign: "center"}}>Already have an account?</Box>
-          <Box sx={{marginTop: "10px"}}>
-            <button onClick={handleCloseModal} className="btnText text-align-center w-100">Log In</button>
+
+        <Box sx={{ marginTop: "30px" }}>
+          <Box sx={{ textAlign: "center" }}>Already have an account?</Box>
+          <Box sx={{ marginTop: "10px" }}>
+            <button
+              onClick={handleCloseModal}
+              className="btnText text-align-center w-100"
+            >
+              Log In
+            </button>
           </Box>
         </Box>
-        
       </Box>
     </Modal>
-    
   );
 }
 
